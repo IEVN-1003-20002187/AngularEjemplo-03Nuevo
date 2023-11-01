3a4482841f6033f3f72249b83b0c602cdba0f2a1
karen <ghkarencita12@gmail.com>
2023-10-31 21:10:14 -0600

    31-10-23_GHDK

---
+++ src/app/formularios/datos-alumno/datos-alumno.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-datos-alumno',
+  templateUrl: './datos-alumno.component.html',
+  styleUrls: ['./datos-alumno.component.css']
+})
+export class DatosAlumnoComponent implements OnInit {
+
+alumnos!: FormGroup;
+
+
+ngOnInit(): void {
+ this.alumnos = new FormGroup({
+  matricula: new FormControl(''),
+  nombre: new FormControl(''),
+  apaterno: new FormControl(''),
+  amaterno: new FormControl(''),
+  correo: new FormControl(''),
+ });  
+}
+}
